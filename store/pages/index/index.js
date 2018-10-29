05f1bd5071e2ec8e608fee788085fe7a48e719db
lvshilin <75869006@qq.com>
2018-10-29 21:07:44 +0800

    Revert "互换"

--- store/pages/index/index.js
+++ store/pages/index/index.js
@@ -4,51 +4,58 @@ const app = getApp()
 
 Page({
   data: {
-    indexUrls: [
-      "../../pic/index/1.jpg",
-      "../../pic/index/2.jpg"
-    ]
+    pro1: "../../static/index/javaee.jpg",
+    pro2: "../../static/index/web.jpg",
+    pro3: "../../static/index/mysql.jpg",
+    pro4: "../../static/index/javawork.jpg",
+    pro5: "../../static/index/wechat.jpg",
+    indexUrls:[
+      "../../static/index/back1.jpg",
+      "../../static/index/back2.jpg"
+    ],
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    openid:''
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  more: function() {
+    wx.showModal({
+      content: '更多热门课程即将推出',
+      showCancel: false,
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        }
+      }
+    });
+  },
+  call: function () {
+    wx.makePhoneCall({
+      phoneNumber: '18362153278',
+      success: function () {
+        console.log("拨打电话成功！")
+      },
+      fail: function () {
+        console.log("拨打电话失败！")
+      }
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    this.getOpenid();
+  },
+  getOpenid() {
+    let that = this;
+    wx.cloud.callFunction({
+      name: 'getOpenid',
+      complete: res => {
+        console.log(res);
+        console.log('云函数获取到的openid: ', res.result.openId)
+        var openid = res.result.openId;
+        that.setData({
+          openid: openid
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
 })
