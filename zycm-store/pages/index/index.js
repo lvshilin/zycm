f7807f32c1cb1cbd7218a529b9af9bd9266a0bd6
lvshilin <75869006@qq.com>
2018-10-29 21:38:25 +0800

    store
    store

---
+++ zycm-store/pages/index/index.js
@@ -0,0 +1,69 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    indexUrls: [
+      "../../pic/index/1.jpg",
+      "../../pic/index/2.jpg"
+    ]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    this.getOpenid();
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  getOpenid() {
+    let that = this;
+    wx.cloud.callFunction({
+      name: 'getOpenid',
+      complete: res => {
+        console.log(res);
+        console.log('云函数获取到的openid: ', res.result.openId)
+        var openid = res.result.openId;
+        that.setData({
+          openid: openid
+        })
+      }
+    })
+  }
+})
